1a390606c9e6cd2f5810d4ca037f7044cbdcdff0
Otávio Borges <otavio.demb@rede.ulbra.br>
2022-05-10 14:09:17 -0300

    projeto atualizado

---
+++ themes/lib/theme.dart
@@ -0,0 +1,85 @@
+import 'package:flutter/material.dart';
+
+final tema = ThemeData(
+  
+  appBarTheme: const AppBarTheme(
+
+    backgroundColor: Color.fromARGB(250, 26, 35, 126),
+
+    titleTextStyle: TextStyle(
+      fontSize: 20,
+      fontWeight: FontWeight.bold,
+      color: Colors.white,
+    ), 
+
+  ),
+
+  floatingActionButtonTheme: const FloatingActionButtonThemeData(
+
+    backgroundColor: Color.fromARGB(250, 255, 179, 0),
+    foregroundColor: Colors.white,
+
+    shape: RoundedRectangleBorder(borderRadius: BorderRadius.all(Radius.circular(2.0)),),
+    elevation: 10,
+  
+  ),
+
+  textTheme: const TextTheme(
+
+    bodyText2: TextStyle(
+      color: Colors.black, 
+      fontStyle: FontStyle.italic,
+      fontSize: 20
+    ),
+
+    headline4: TextStyle(
+      color: Color.fromARGB(250, 26, 35, 126),
+      fontWeight: FontWeight.bold, 
+      fontSize: 40
+    ),
+  ),
+  
+);
+
+//Tema Noturno
+
+ThemeData temaNoturno = ThemeData(
+  brightness: Brightness.dark,
+  
+  appBarTheme: const AppBarTheme(
+
+    backgroundColor: Color.fromARGB(250, 26, 35, 126),
+
+    titleTextStyle: TextStyle(
+      fontSize: 20,
+      fontWeight: FontWeight.bold,
+      color: Colors.white,
+    ), 
+
+  ),
+
+  floatingActionButtonTheme: const FloatingActionButtonThemeData(
+
+    backgroundColor: Color.fromARGB(250, 26, 35, 126), 
+    foregroundColor: Colors.white,
+
+    shape: RoundedRectangleBorder(borderRadius: BorderRadius.all(Radius.circular(2.0)),),
+    elevation: 10,
+  
+  ),
+
+  textTheme: const TextTheme(
+
+    bodyText2: TextStyle(
+      color: Colors.white, 
+      fontStyle: FontStyle.italic,
+      fontSize: 20
+    ),
+
+    headline4: TextStyle(
+      color: Color.fromARGB(250, 255, 179, 0),
+      fontWeight: FontWeight.bold, 
+      fontSize: 40
+    ),
+  ),
+);
